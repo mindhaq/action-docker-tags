cc47ec419e309662bb742aa8d174ceee0f5a067d
Rüdiger Schulz <mindhaq@users.noreply.github.com>
2019-12-19 00:28:19 +0100

    return docker_hash with 7 digits

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const ref = process.env['GITHUB_REF'];
 const sha = process.env['GITHUB_SHA'];
 
 const docker_branch = ref.substring(ref.lastIndexOf('/') + 1);
-const docker_hash = sha.substring(0, 6);
+const docker_hash = sha.substring(0, 7);
 const docker_version = `${docker_branch}.${docker_hash}`;
 const docker_tag = `${project}:${docker_version}`;
 
 core.setOutput('docker_branch', docker_branch);
+core.setOutput('docker_hash', docker_hash);
 core.setOutput('docker_version', docker_version);
 core.setOutput('docker_tag', docker_tag);
 
